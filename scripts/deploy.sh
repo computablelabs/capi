db3fbe4687815570df2fef05e770ce21ac75887f
Will Button <will@willbutton.co>
2019-09-05 14:19:53 -0700

    remove testing values from config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,10 @@
 
 # Build and tag docker image
 docker build -t "capi:latest" .
-docker tag "capi:latest" "$REPO_URI:capi" #TODO: change to REPO_URI:latest prior to merging
+docker tag "capi:latest" "$REPO_URI:latest"
 docker tag "capi:latest" "$REPO_URI:$TRAVIS_COMMIT"
 $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
-echo "pushing $REPO_URI:capi to ECR"
-docker push "$REPO_URI:capi" #TODO: change to REPO_URI:latest prior to merging
+docker push "$REPO_URI:latest"
 docker push "$REPO_URI:$TRAVIS_COMMIT"
 
 # Force deploy service to pick up new docker image & wait for success
