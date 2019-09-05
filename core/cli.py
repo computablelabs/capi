7d5f343b6b83dfbcdb14dc74096c1939a0b78f7b
robrobbins <rob.robb.ns@gmail.com>
2019-09-05 11:53:07 -0700

    implement admin cli register

--- core/cli.py
+++ core/cli.py
@@ -3,24 +3,33 @@ Blueprint containing the 'admin' CLI for datatrust operators
 """
 from flask import Blueprint
 from flask import current_app, g
-from core.protocol import get_datatrust
-from computable.helpers.transaction import call, send
+from .protocol import set_w3, get_datatrust
+from .helpers import set_gas_prices
+from computable.helpers.transaction import call, transact, send
 
 admin = Blueprint('admin', __name__)
 
 @admin.cli.command('register')
-def register():
+@click.argument('gas_price')
+def register(gas_price):
     """
     register as this market's datatrust
     """
-    # TODO likely will need to call protocol.set_w3 by hand as this is not in
-    # a request cycle
-    # dt = get_datatrust()
-    # tx = send(dt.register...
-    # rct = g.w3.waitForTransactionReceipt(tx)
-    # return rct
-    # TODO this
-    pass
+    # set_w3 by hand as this is not in a request cycle
+    set_w3()
+    dt = get_datatrust()
+    # comp.py HOC methods produce a tuple -> (tx, opts)
+    t = dt.register(current_app.config['DNS_NAME'])
+    # we use an abstracted helper to estimate gas, and set the given gas price
+    args = set_gas_prices(t, gas_price) # omit gas arg and it will be estimated
+    # TEST env never need use send...
+    if current_app.config['TESTING'] == True:
+        tx = transact(args)
+    else:
+        tx = send(g.w3, current_app.config['PRIVATE_KEY'], args)
+
+    rct = g.w3.waitForTransactionReceipt(tx)
+    return rct
 
 @admin.cli.command('get_candidate')
 def get_candidate():
---
+++ core/helpers.py
@@ -0,0 +1,23 @@
+from flask import current_app, g
+
+def set_gas_prices(t, gas_price, gas=None):
+    """
+    Given a Computable.py HOC tuple, a gas price and an optional gas amount, set them
+    into the 'txOpts' (t[1])
+    """
+    if gas == None:
+        est = t[0].estimateGas()
+    # just in case its lower, defer to anything there...
+    t[1]['gas'] = max(t[1]['gas'], est)
+    t[1]['gas_price'] = gas_price
+    return t
+
+def get_gas_price(key='average'):
+    """
+    Ethgasstation has an api endpoint that will return current estimates for pricing
+    GET https://ethgasstation.info/json/ethgasAPI.json
+    param optional key should be one of:
+        ['safelow','average','fast','fastest']
+    TODO this...
+    """
+    pass
