f5101ebb641b0d10c5e7fef0d1abee244129042c
robrobbins <rob.robb.ns@gmail.com>
2019-10-09 10:32:58 -0700

    specage

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,33 @@
 import pytest
 from computable.helpers.transaction import call
+from apis.helpers import extract_listing_hashes, extract_listing_hashes_to_block, listing_hash_join
 from tests.helpers import maybe_transfer_market_token, maybe_increase_market_token_approval
+
+@pytest.fixture(scope='module')
+def logs(w3):
+    return [
+        {
+            'name': 'FooEvent',
+            'blockNumber': 1,
+            'args': {
+            'hash': w3.toBytes(hexstr=w3.toHex(text='foo'))
+            }
+        },
+        {
+            'name': 'BarEvent',
+            'blockNumber': 2,
+            'args': {
+            'hash': w3.toBytes(hexstr=w3.toHex(text='bar'))
+            }
+        },
+        {
+            'name': 'BazEvent',
+            'blockNumber': 4,
+            'args': {
+            'hash': w3.toBytes(hexstr=w3.toHex(text='baz'))
+            }
+        },
+    ]
 
 def test_maybe_transfer_market_token(w3, market_token):
     user = w3.eth.accounts[1]
@@ -23,3 +50,37 @@ def test_maybe_increase_market_token_approval(w3, market_token, voting):
     tx_rct = maybe_increase_market_token_approval(w3, market_token, user, voting.address, one_gwei)
     allowed = call(market_token.allowance(user, voting.address))
     assert allowed == one_gwei
+
+def test_extract_listing_hashes(logs):
+    extracted = extract_listing_hashes(logs)
+    assert len(extracted) == 3
+
+def test_extract_listing_hashes_to_block_no_filter_by(logs):
+    extracted, tb = extract_listing_hashes_to_block(logs)
+    assert len(extracted) == 3
+    assert tb == 4
+
+def test_extract_listing_hashes_to_block_filter_by_no_matches(w3, logs):
+    filter_by = [w3.toHex(text='spam'), w3.toHex(text='eggs')]
+    extracted, tb = extract_listing_hashes_to_block(logs, filter_by)
+    assert len(extracted) == 3
+    assert tb == 4
+
+def test_extract_listing_hashes_to_block_filter_by_one_match(w3, logs):
+    filter_by = [w3.toHex(text='foo')]
+    extracted, tb = extract_listing_hashes_to_block(logs, filter_by)
+    assert len(extracted) == 2
+    assert tb == 4
+
+def test_extract_listing_hashes_to_block_filter_by_two_matches(w3, logs):
+    filter_by = [w3.toHex(text='bar'), w3.toHex(text='baz')]
+    extracted, tb = extract_listing_hashes_to_block(logs, filter_by)
+    assert len(extracted) == 1
+    assert tb == 4
+
+
+def test_extract_listing_hashes_to_block_filter_by_three_matches(w3, logs):
+    filter_by = extract_listing_hashes(logs)
+    extracted, tb = extract_listing_hashes_to_block(logs, filter_by)
+    assert len(extracted) == 0
+    assert tb == 4
