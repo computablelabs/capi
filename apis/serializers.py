ffed992885eb7439c82f2416698bd46f0bad50fe
Will Button <will@willbutton.co>
2019-10-22 15:33:29 -0700

    add size to listing

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,6 +1,7 @@
 """
 Some serializer models will be useful to multiple namespaces
 """
+from flask import current_app, g
 from flask_restplus import Model, fields
 
 Listing = Model('Listing', {
@@ -10,7 +11,8 @@ Listing = Model('Listing', {
     'listing_hash': fields.String(required=True, description='The listing this upload belongs to'),
     'file_type': fields.String(required=True, description='File content: video, image, audio, etc'),
     'tags': fields.List(fields.String, required=False, description='Tags to categorize and describe the listing file'),
-    'owner': fields.String(required=True, description='Public key of the owner')
+    'owner': fields.String(required=True, description='Public key of the owner'),
+    'size': fields.Integer(required=True, description='Size of listing (in bytes)')
     })
 
 Listings = Model('Listings', {
@@ -18,3 +20,16 @@ Listings = Model('Listings', {
     'from_block': fields.Integer(required=True, description='Block number where scanning began'),
     'to_block': fields.Integer(required=True, description='Highest block number scanned')
     })
+
+def file_size_in_s3(key):
+    """
+    Return the size of an object stored in s3
+    """
+    s3_object = g.s3.head_object(
+        Bucket=current_app.config['S3_DESTINATION'],
+        Key=key
+    )
+    if 'ContentLength' in s3_object:
+        return s3_object['ContentLength']
+    else:
+        return None
