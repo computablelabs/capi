81678caf77d3952aa19c25c1dd7aed8672008bce
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2019-10-22 15:46:55 -0700

    Fixed registration test

--- tests/cli/test_register.py
+++ tests/cli/test_register.py
@@ -1,16 +1,70 @@
 from flask import current_app
 import pytest
 from core.constants import REGISTERED_CANDIDATE
-from computable.helpers.transaction import call
+from computable.helpers.transaction import call, transact
 
-def test_register(voting, datatrust, ctx):
+def test_register(w3, ether_token, market_token, voting, parameterizer, reserve, datatrust, ctx):
+    user = w3.eth.defaultAccount 
+    user_bal = call(ether_token.balance_of(user))
+    assert user_bal == 0
+
+    # Deposit ETH in EtherToken
+    tx = transact(ether_token.deposit(
+        w3.toWei(10, 'ether'), {'from': user}))
+    rct = w3.eth.waitForTransactionReceipt(tx)
+    new_user_bal = call(ether_token.balance_of(user))
+    assert new_user_bal == w3.toWei(10, 'ether')
+    assert rct['status'] == 1
+
+    # Approve the spend
+    old_allowance = call(ether_token.allowance(user, reserve.address))
+    assert old_allowance == 0
+    tx= transact(ether_token.approve(reserve.address, w3.toWei(10, 'ether'), opts={'from': user}))
+    rct = w3.eth.waitForTransactionReceipt(tx)
+    assert rct['status'] == 1
+    new_allowance = call(ether_token.allowance(user, reserve.address))
+    assert new_allowance == w3.toWei(10, 'ether')
+
+    # Perform pre-checks for support 
+    support_price = call(reserve.get_support_price())
+    assert new_user_bal >= support_price
+    assert new_allowance >= new_user_bal
+    minted = (new_user_bal // support_price) * 10**9
+    assert minted == 10**7 * w3.toWei(1, 'gwei')
+    priv = call(market_token.has_privilege(reserve.address))
+    assert priv == True
+    total_supply = call(market_token.total_supply())
+    assert total_supply == w3.toWei(4, 'ether')
+
+    # Call support
+    tx = transact(reserve.support(new_user_bal, opts={'gas': 1000000, 'from': user}))
+    rct = w3.eth.waitForTransactionReceipt(tx)
+    assert rct['status'] == 1
+    logs = reserve.deployed.events.Supported().processReceipt(rct)
+    cmt_user_bal = call(market_token.balance_of(user))
+    # There is the creator already
+    assert cmt_user_bal >= w3.toWei(10, 'milliether')
+    new_supply = call(market_token.total_supply())
+    assert new_supply == total_supply + w3.toWei(10, 'milliether')
+
+    stake = call(parameterizer.get_stake())
+    assert stake <= cmt_user_bal
+
+    # Approve the market token allowance
+    old_mkt_allowance = call(market_token.allowance(user, voting.address))
+    assert old_mkt_allowance == 0
+    tx = transact(market_token.approve(voting.address, w3.toWei(10, 'milliether'), opts={'from': user}))
+    rct = w3.eth.waitForTransactionReceipt(tx)
+    assert rct['status'] == 1
+    new_mkt_allowance = call(market_token.allowance(user, voting.address))
+    assert new_mkt_allowance == w3.toWei(10, 'milliether')
+    assert stake <= new_mkt_allowance
+
     runner = current_app.test_cli_runner()
     result = runner.invoke(args=['admin', 'registration_test'])
-    # TODO: The swap to staked registration means that registering with the register CLI command fails without CMT for staking. Fix the registration test to stake properly so the registered candidate is actually in output
-    assert REGISTERED_CANDIDATE not in result.output
+    assert REGISTERED_CANDIDATE in result.output
 
 def test_registered_candidate(w3, voting, ctx):
     hash = w3.keccak(text=current_app.config['DNS_NAME'])
     candidate = call(voting.is_candidate(hash))
-    # TODO:This should be fixed so the candidate is actually made once registration test above is fixed
-    assert candidate == False 
+    assert candidate == True
