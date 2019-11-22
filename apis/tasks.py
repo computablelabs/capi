3d9b58f22f922fa2f2969d77b70fd254ce863a60
robrobbins <rob.robb.ns@gmail.com>
2019-11-22 12:09:40 -0700

    LIES. LIES. LIES.

--- apis/tasks.py
+++ apis/tasks.py
@@ -3,6 +3,7 @@ Asyncronous tasks that may be used by more than one namespace
 """
 from time import sleep
 from flask import current_app, g
+from web3.exceptions import TransactionNotFound
 from app import celery
 from core.protocol import set_w3
 import core.constants as C
@@ -28,11 +29,19 @@ def wait_for_mining(tx_hash):
 
     # NOTE that price_and_time[1] is 2x what the ethGasStation estimate is
     while slept < price_and_time[1]:
-        tx_rcpt = g.w3.eth.getTransactionReceipt(tx_hash)
+        # because web3 throws if not present vs returning None (like the docs say)
+        try:
+            tx_rcpt = g.w3.eth.getTransactionReceipt(tx_hash)
+        except TransactionNotFound:
+            tx_rcpt = None
+            current_app.logger.info(f'Transaction Receipt not ready after {slept} seconds, sleeping...')
+        except:
+            tx_rcpt = None
+            current_app.logger.info(f'Unexpected error looking up transaction after {slept} seconds, sleeping...')
+
         if tx_rcpt != None:
             break
         slept = slept + C.TRANSACTION_RETRY
-        current_app.logger.info(f'Transaction Receipt not ready after {slept} seconds, sleeping...')
         sleep(C.TRANSACTION_RETRY)
 
     if tx_rcpt == None:
